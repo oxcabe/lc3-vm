646fca93a9472805030c15236bf1061729b52bac
Óscar Carrasco <oxcabe@gmail.com>
2020-02-18 22:14:21 +0000

    Implement opcode selection

--- lc3_vm.c
+++ lc3_vm.c
@@ -89,7 +89,38 @@ int main(int argc, char **argv) {
 		reg[R_PC]++;
 
 		switch (op) {
-
+		case OP_ADD:
+			break;
+		case OP_AND:
+			break;
+		case OP_NOT:
+			break;
+		case OP_BR:
+			break;
+		case OP_JMP:
+			break;
+		case OP_JSR:
+			break;
+		case OP_LD:
+			break;
+		case OP_LDI:
+			break;
+		case OP_LDR:
+			break;
+		case OP_LEA:
+			break;
+		case OP_ST:
+			break;
+		case OP_STI:
+			break;
+		case OP_STR:
+			break;
+		case OP_TRAP:
+			break;
+		case OP_RES:
+		case OP_RTI:
+		default:
+			break;
 		}
 	}
 }
